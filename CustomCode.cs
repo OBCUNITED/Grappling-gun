ab88aef59dd9edf20f12aff578756d41e8a0e233
OBCUNITED <75881123+OBCUNITED@users.noreply.github.com>
2020-12-12 12:56:29 +0300

    Create CustomCode.cs

---
+++ CustomCode.cs
@@ -0,0 +1,15 @@
+// You dont need to use that code
+
+using System;
+using UnityEngine;
+    
+    public class Denebunu : MonoBehaviour{
+        public void Handle;
+        private bool Point;
+        public void whatispoint;
+
+         public Vector speed = 30f;
+         public Vector damage = 45f
+    }
+
+}
